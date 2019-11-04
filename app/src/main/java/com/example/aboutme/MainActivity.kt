80dadef6011701ac6b000871ff8811bc5231fe22
Kimberly McGimpsey <kmcgimpsey@aol.com>
2019-11-04 13:05:45 -0800

    Added some code in MainActivity and code to hide the keyboard.

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,12 +1,41 @@
 package com.example.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var editText: EditText
+    lateinit var nicknameTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.done_button).setOnClickListener {
+                addNickname(it)
+        }
+
+        editText = findViewById(R.id.nickname_edit)
+        nicknameTextView = findViewById(R.id.nickname_text)
     }
+
+    private fun addNickname(view: View) {
+        nicknameTextView.text = editText.text
+        editText.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE
+
+        //hide the keyboard
+        val inn = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inn.hideSoftInputFromWindow(view.windowToken, 0)
+
+        }
+
 }
